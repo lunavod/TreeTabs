004624084af5beb993e0eb6a41c28dce8096b12c
Yegor Pokrovskiy <lunavod@bk.ru>
2024-03-03 22:19:44 +0200

    feat: add pwa capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 import reactCssModule from "./vite-tools/react-css-modules";
 import sassDts from "vite-plugin-sass-dts";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 const generateScopedName = "[path]___[name]__[local]";
 export default defineConfig({
@@ -26,6 +27,46 @@ export default defineConfig({
         ".css": {
           syntax: "postcss",
         },
+      },
+    }),
+    VitePWA({
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      // add this to cache all the
+      // static assets in the public folder
+      includeAssets: ["**/*"],
+      registerType: "autoUpdate",
+      manifest: {
+        theme_color: "#44454d",
+        background_color: "#44454d",
+        display: "browser",
+        scope: "/",
+        start_url: "/",
+        name: "TreeTabs",
+        short_name: "TreeTabs",
+        icons: [
+          {
+            src: "app_icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "app_icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "app_icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "app_icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
